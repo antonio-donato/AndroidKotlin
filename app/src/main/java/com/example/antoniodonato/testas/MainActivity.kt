92a994763c7350ee1a45b677546c0c068cb0879b
Antonio Donato <antonio.donato@cgm.com>
2019-01-17 22:08:39 +0100

    Ricompilato con nuova versione Android Studio

--- app/src/main/java/com/example/antoniodonato/testas/MainActivity.kt
+++ app/src/main/java/com/example/antoniodonato/testas/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.antoniodonato.testas
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "Ciao")
     }
     fun toastMe(view: View){
         val myToast = Toast.makeText(this, "Ciao Toast!", Toast.LENGTH_SHORT)
